7a6a55ff73a537cf8a2714490f09cfa22a0b2c64
martin bedouret <martinbedouret@gmail.com>
2020-09-10 12:58:23 -0300

    wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,34 +7,26 @@ export default function Footer() {
       <div className="container">
         <p>&copy; Cboard 2020. All Rights Reserved.</p>
         <ul className="list-inline">
-          <li className="list-inline-item">
-            <a href="/#">Privacy</a>
+          <li className="list-inline-item mr-3">
+            <Link to="/privacy/">Privacy</Link>
+          </li>
+          <li className="list-inline-item mr-3">
+            <Link to="/terms/">Terms</Link>
+          </li>
+          <li className="list-inline-item mr-3">
+            <Link to="/help/">FAQ</Link>
+          </li>
+          <li className="list-inline-item mr-3">
+            <Link to="/about/">About</Link>
+          </li>
+          <li className="list-inline-item mr-3">
+            <Link to="/help/">Help and Support</Link>
+          </li>
+          <li className="list-inline-item mr-3">
+            <Link to="/blog/">Blog</Link>
           </li>
           <li className="list-inline-item">
-            <a href="/#">Terms</a>
-          </li>
-          <li className="list-inline-item">
-            <a href="/#">FAQ</a>
-          </li>
-          <li className="list-inline-item">
-            <Link to="/about/">
-              About
-                  </Link>
-          </li>
-          <li className="list-inline-item">
-            <Link to="/help/">
-              Help and Support
-                  </Link>
-          </li>
-          <li className="list-inline-item">
-            <Link to="/blog/">
-              Blog
-                  </Link>
-          </li>
-          <li className="list-inline-item">
-            <Link to="/donate/">
-              Donate
-                  </Link>
+            <Link to="/donate/">Donate</Link>
           </li>
         </ul>
       </div>
