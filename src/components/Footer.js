f15ca283feb4d18be6f3fb9ef910ca9f0923f420
martin bedouret <martinbedouret@gmail.com>
2020-09-09 22:10:24 -0300

    wip

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <p>&copy; Gatsby Starter New Age 2019. All Rights Reserved.</p>
+        <p>&copy; Cboard 2020. All Rights Reserved.</p>
         <ul className="list-inline">
           <li className="list-inline-item">
             <a href="/#">Privacy</a>
@@ -14,6 +15,26 @@ export default function Footer() {
           </li>
           <li className="list-inline-item">
             <a href="/#">FAQ</a>
+          </li>
+          <li className="list-inline-item">
+            <Link to="/about/">
+              About
+                  </Link>
+          </li>
+          <li className="list-inline-item">
+            <Link to="/help/">
+              Help and Support
+                  </Link>
+          </li>
+          <li className="list-inline-item">
+            <Link to="/blog/">
+              Blog
+                  </Link>
+          </li>
+          <li className="list-inline-item">
+            <Link to="/donate/">
+              Donate
+                  </Link>
           </li>
         </ul>
       </div>
