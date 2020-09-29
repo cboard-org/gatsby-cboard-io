0c6a3ca7f8f65ed1519cc2c3ce74959029b0bcf4
martin bedouret <martinbedouret@gmail.com>
2020-09-29 12:39:57 -0300

    wip

--- src/pages/help.js
+++ src/pages/help.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { Form, FormControl } from "react-bootstrap"
 import PropTypes from 'prop-types';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
@@ -18,9 +19,20 @@ const Help = ({ data }) => {
         article
       />
       <header className="helphead">
-        <div className="container">
-          <h2 className="mb-2">Cboard Help</h2>
-          <p>We are here to help. Make Cboard yours.</p>
+        <div className="container h-100">
+          <div className="row h-100">
+            <div className="header-content my-auto mx-auto">
+              <h1 className="mb-2">Cboard Help</h1>
+              <h5>We are here to help. Make Cboard yours.</h5>
+              <Form className="help-filter">
+                <FormControl
+                  className="bg-none search"
+                  type="text"
+                  placeholder="Búsqueda"
+                />
+              </Form>
+            </div>
+          </div>
         </div>
       </header>
       <section className="container">
